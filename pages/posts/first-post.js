79a4e2cbdace4bfab9fcf68dd6b9970ecf5e7036
Harris <harriskarume.hk@gmail.com>
2023-04-18 12:19:55 +0300

    Assets, metadata, css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,23 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+
+            <Image
+                src='/images/profile.jpg'
+                height={150}
+                width={150}
+            ></Image>
+
             <h1>First Post</h1>
-            <h2>Back <Link href='/'>home</Link></h2>
-        </>
+            <h2><Link href='/'>Back home</Link></h2>
+        </Layout>
     )
 }
