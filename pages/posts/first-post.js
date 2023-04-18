d6638ac4d450240c7bbe7297937ba2c4ea8fca30
Harris <harriskarume.hk@gmail.com>
2023-04-18 08:48:42 +0300

    Link pages using <Link />

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,10 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
     return (
-        <h1>First Post</h1>
+        <>
+            <h1>First Post</h1>
+            <h2>Back <Link href='/'>home</Link></h2>
+        </>
     )
 }
